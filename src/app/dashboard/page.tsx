4382080330a629a3e31feaea7b270fdee776f2a9
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-12-07 22:04:34 +0200

    Appointments component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import useAuth from "@/utils/auth";
 import { useEffect, useState } from "react";
 import { useAuthContext } from "@/context/authContext";
 import { addAppointment, readData, writeUsers } from "../../../firebase/client";
+import Appointments from "@/components/appointments";
 
 export default function Dashboard() {
   const router = useRouter();
@@ -28,17 +29,6 @@ export default function Dashboard() {
     return <div>Loading...</div>;
   }
 
-  const handleAddAppointment = (e: any) => {
-    e.preventDefault();
-    addAppointment(1, newAppointment);
-    setNewAppointment({
-      title: "",
-      doctor: "",
-      date: "",
-      notes: "",
-    });
-  };
-
   readData(1);
 
   return (
@@ -61,108 +51,12 @@ export default function Dashboard() {
           <div className="col-span-10 pe-4">
             <div className="grid grid-rows-12 gap-4 h-full py-3">
               <div className="row-span-6">
-                <div className="appointments h-full px-8 py-5">
-                  <h2>Appointments</h2>
-                  <div className="mt-8">
-                    {showForm ? (
-                      <div className="relative w-[300px]">
-                        <form>
-                          <div>
-                            <label htmlFor="title">Title</label>
-                            <input
-                              type="text"
-                              id="title"
-                              value={newAppointment.title}
-                              onChange={(e) =>
-                                setNewAppointment({
-                                  ...newAppointment,
-                                  title: e.target.value,
-                                })
-                              }
-                            />
-                          </div>
-                          <div>
-                            <label htmlFor="doctor">Doctor</label>
-                            <input
-                              type="text"
-                              id="doctor"
-                              value={newAppointment.doctor}
-                              onChange={(e) =>
-                                setNewAppointment({
-                                  ...newAppointment,
-                                  doctor: e.target.value,
-                                })
-                              }
-                            />
-                          </div>
-                          <div>
-                            <label htmlFor="date">Date</label>
-                            <input
-                              type="date"
-                              id="date"
-                              value={newAppointment.date}
-                              onChange={(e) =>
-                                setNewAppointment({
-                                  ...newAppointment,
-                                  date: e.target.value,
-                                })
-                              }
-                            />
-                          </div>
-                          <div>
-                            <label htmlFor="notes">Notes</label>
-                            <textarea
-                              id="notes"
-                              value={newAppointment.notes}
-                              onChange={(e) =>
-                                setNewAppointment({
-                                  ...newAppointment,
-                                  notes: e.target.value,
-                                })
-                              }
-                            />
-                          </div>
-                          <button onClick={(e) => handleAddAppointment(e)}>
-                            Create appointment
-                          </button>
-                        </form>
-                        <button
-                          className="hide-form-btn"
-                          onClick={() => setShowForm((prv) => !prv)}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="20px"
-                            height="20px"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                          >
-                            <g id="Menu / Close_MD">
-                              <path
-                                id="Vector"
-                                d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18"
-                                stroke="#000000"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                              />
-                            </g>
-                          </svg>
-                        </button>
-                      </div>
-                    ) : (
-                      <>
-                        <p>Your appointments will be displayed here</p>
-                        <button
-                          className="mt-4"
-                          onClick={() => setShowForm((prv) => !prv)}
-                        >
-                          Create an appointment
-                        </button>
-                      </>
-                    )}
-                  </div>
-                </div>
+                <Appointments
+                  showForm={showForm}
+                  setShowForm={setShowForm}
+                  newAppointment={newAppointment}
+                  setNewAppointment={setNewAppointment}
+                />
               </div>
               <div className="row-span-6">
                 <div className="medication h-full px-8 py-5">
---
+++ src/components/appointments.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { addAppointment } from "../../firebase/client";
+
+interface Data {
+  showForm: boolean;
+  setShowForm: any;
+  newAppointment: any;
+  setNewAppointment: any;
+}
+
+export default function Appointments({
+  showForm,
+  setShowForm,
+  newAppointment,
+  setNewAppointment,
+}: Data) {
+  const handleAddAppointment = (e: any) => {
+    e.preventDefault();
+    addAppointment(1, newAppointment);
+    setNewAppointment({
+      title: "",
+      doctor: "",
+      date: "",
+      notes: "",
+    });
+  };
+
+  return (
+    <>
+      <div className="appointments h-full px-8 py-5">
+        <h2>Appointments</h2>
+        <div className="mt-8">
+          {showForm ? (
+            <div className="relative w-[300px]">
+              <form>
+                <div>
+                  <label htmlFor="title">Title</label>
+                  <input
+                    type="text"
+                    id="title"
+                    value={newAppointment.title}
+                    onChange={(e) =>
+                      setNewAppointment({
+                        ...newAppointment,
+                        title: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <div>
+                  <label htmlFor="doctor">Doctor</label>
+                  <input
+                    type="text"
+                    id="doctor"
+                    value={newAppointment.doctor}
+                    onChange={(e) =>
+                      setNewAppointment({
+                        ...newAppointment,
+                        doctor: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <div>
+                  <label htmlFor="date">Date</label>
+                  <input
+                    type="date"
+                    id="date"
+                    value={newAppointment.date}
+                    onChange={(e) =>
+                      setNewAppointment({
+                        ...newAppointment,
+                        date: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <div>
+                  <label htmlFor="notes">Notes</label>
+                  <textarea
+                    id="notes"
+                    value={newAppointment.notes}
+                    onChange={(e) =>
+                      setNewAppointment({
+                        ...newAppointment,
+                        notes: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <button onClick={(e) => handleAddAppointment(e)}>
+                  Create appointment
+                </button>
+              </form>
+              <button
+                className="hide-form-btn"
+                onClick={() => setShowForm((prv: boolean) => !prv)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20px"
+                  height="20px"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <g id="Menu / Close_MD">
+                    <path
+                      id="Vector"
+                      d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18"
+                      stroke="#000000"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </g>
+                </svg>
+              </button>
+            </div>
+          ) : (
+            <>
+              <p>Your appointments will be displayed here</p>
+              <button
+                className="mt-4"
+                onClick={() => setShowForm((prv: boolean) => !prv)}
+              >
+                Create an appointment
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
