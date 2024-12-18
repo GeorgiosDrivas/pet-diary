8544e0bb0c157f3692b559e0403ab488c9fe6765
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-12-18 16:36:00 +0200

    Logout functionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -73,23 +73,7 @@ export default function Dashboard() {
               </div>
               <div className="flex justify-between items-center row-span-1">
                 <p>{user?.displayName || "Guest"}</p>
-                <svg
-                  onClick={() => Logout()}
-                  className="me-4 cursor-pointer"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30px"
-                  height="30px"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="M15 16.5V19C15 20.1046 14.1046 21 13 21H6C4.89543 21 4 20.1046 4 19V5C4 3.89543 4.89543 3 6 3H13C14.1046 3 15 3.89543 15 5V8.0625M11 12H21M21 12L18.5 9.5M21 12L18.5 14.5"
-                    stroke="#000000"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
+                <Logout />
               </div>
             </div>
           </div>
