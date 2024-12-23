bb684e992020d35410f838272935c59235cca7b1
Georgios Drivas <drivasgeorgios0@gmail.com>
2024-12-23 21:18:15 +0200

    New pet button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -64,6 +64,22 @@ export default function Dashboard() {
                       {pet.name}
                     </p>
                   ))}
+                <svg
+                  className="new-pet-svg cursor-pointer"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20px"
+                  height="20px"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <path
+                    d="M6 12H18M12 6V18"
+                    stroke="#000000"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
               </div>
               <div className="flex justify-between items-center row-span-1">
                 <p>{user?.displayName || "Guest"}</p>
