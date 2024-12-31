61b6ae8a291a31f5b3b48b6e142d0f3cb9f4843c
Georgios Drivas <drivasgeorgios0@gmail.com>
2024-12-31 18:39:53 +0200

    Refactor medication & appointment components part I

---
+++ src/assets/closeSvg.tsx
@@ -0,0 +1,24 @@
+export default function CloseSvg() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20px"
+        height="20px"
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <g id="Menu / Close_MD">
+          <path
+            id="Vector"
+            d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18"
+            stroke="#000000"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </g>
+      </svg>
+    </>
+  );
+}
