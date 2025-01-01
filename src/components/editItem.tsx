f579b16d6457148b4343a74998d5fbec7a7410ae
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-01-01 16:51:14 +0200

    Edit item Form layout

---
+++ src/components/editItem.tsx
@@ -0,0 +1,27 @@
+export default function EditItem() {
+  return (
+    <>
+      <div className="relative w-[300px]">
+        <form>
+          <div>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" />
+          </div>
+          <div>
+            <label htmlFor="doctor">Doctor</label>
+            <input type="text" id="doctor" />
+          </div>
+          <div>
+            <label htmlFor="date">Date</label>
+            <input type="date" id="date" />
+          </div>
+          <div>
+            <label htmlFor="notes">Notes</label>
+            <textarea id="notes" />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
+}
