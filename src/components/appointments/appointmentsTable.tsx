59e28f6bb52a8c77fafb905100d0e6dd0dd48810
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-01-08 18:24:54 +0200

    Edit item refactor

--- src/components/appointments/appointmentsTable.tsx
+++ src/components/appointments/appointmentsTable.tsx
@@ -2,12 +2,13 @@ import DeleteSvg from "@/assets/deleteSvg";
 import EditSvg from "@/assets/editSvg";
 import { AppointmentsType, Pet } from "@/types";
 import { removeAppointment } from "../../../firebase/deleteMethods";
-import React from "react";
+import React, { useState } from "react";
+import EditItem from "../editItem";
 
 export default function AppointmentsTable({ pet }: { pet: Pet }) {
-  //   const [editItem, setEditItem] = useState(false);
-  //   const [editableAppointment, setEditableAppointment] =
-  //     useState<AppointmentsType | null>(null);
+  const [editItem, setEditItem] = useState(false);
+  const [editableAppointment, setEditableAppointment] =
+    useState<AppointmentsType | null>(null);
 
   const removeAppointmentFn = (pet: Pet, index: number) => {
     window.location.reload();
@@ -15,56 +16,63 @@ export default function AppointmentsTable({ pet }: { pet: Pet }) {
     alert("Appointment removed successfully.");
   };
 
-  //   const editAppointment = (appointment: AppointmentsType) => {
-  //     // setEditItem(true);
-  //     // setEditableAppointment(appointment);
-  //   };
+  const editAppointment = (appointment: AppointmentsType) => {
+    setEditItem(true);
+    setEditableAppointment(appointment);
+  };
 
   return (
     <>
-      <table className="w-full border-collapse border border-gray-200">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Doctor</th>
-            <th>Date</th>
-            <th>Notes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {pet.appointments.length > 0 ? (
-            pet.appointments.map(
-              (appointment: AppointmentsType, index: number) => (
-                <tr key={index} className="border-b border-[#e5e7eb]">
-                  <td className="text-center py-3">{appointment.title}</td>
-                  <td className="text-center py-3">{appointment.doctor}</td>
-                  <td className="text-center py-3">{appointment.date}</td>
-                  <td className="text-center py-3">
-                    {appointment.notes || "No notes"}
-                  </td>
-                  <td className="text-center py-3">
-                    <button className="me-3 my-2">
-                      <EditSvg />
-                    </button>
-                    <button
-                      className="my-2"
-                      onClick={() => removeAppointmentFn(pet, index)}
-                    >
-                      <DeleteSvg />
-                    </button>
-                  </td>
-                </tr>
-              )
-            )
-          ) : (
+      {editItem ? (
+        <EditItem appointment={editableAppointment} />
+      ) : (
+        <table className="w-full border-collapse border border-gray-200">
+          <thead>
             <tr>
-              <td colSpan={4} className="text-center">
-                No appointments available.
-              </td>
+              <th>Title</th>
+              <th>Doctor</th>
+              <th>Date</th>
+              <th>Notes</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {pet.appointments.length > 0 ? (
+              pet.appointments.map(
+                (appointment: AppointmentsType, index: number) => (
+                  <tr key={index} className="border-b border-[#e5e7eb]">
+                    <td className="text-center py-3">{appointment.title}</td>
+                    <td className="text-center py-3">{appointment.doctor}</td>
+                    <td className="text-center py-3">{appointment.date}</td>
+                    <td className="text-center py-3">
+                      {appointment.notes || "No notes"}
+                    </td>
+                    <td className="text-center py-3">
+                      <button
+                        className="me-3 my-2"
+                        onClick={() => editAppointment(appointment)}
+                      >
+                        <EditSvg />
+                      </button>
+                      <button
+                        className="my-2"
+                        onClick={() => removeAppointmentFn(pet, index)}
+                      >
+                        <DeleteSvg />
+                      </button>
+                    </td>
+                  </tr>
+                )
+              )
+            ) : (
+              <tr>
+                <td colSpan={4} className="text-center">
+                  No appointments available.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      )}
     </>
   );
 }
