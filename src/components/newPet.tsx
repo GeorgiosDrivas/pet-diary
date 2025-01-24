6b79c62a5f395bb9b909cd3307e3718c81f2399e
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-01-24 21:28:55 +0200

    Refactor state in newPet

--- src/components/newPet.tsx
+++ src/components/newPet.tsx
@@ -3,22 +3,26 @@ import { addPet } from "../../firebase/addMethods";
 import { Pet } from "@/types";
 
 export default function NewPet() {
-  const [name, setName] = useState("");
-  const [species, setSpecies] = useState("");
-  const [breed, setBreed] = useState("");
-  const [age, setAge] = useState("");
+  const [newPetState, setNewPetState] = useState({
+    name: "",
+    species: "",
+    breed: "",
+    age: "",
+  });
 
   const newPet: Pet = {
-    name: name,
-    species: species,
-    breed: breed,
-    age: age,
+    name: newPetState.name,
+    species: newPetState.species,
+    breed: newPetState.breed,
+    age: newPetState.age,
     appointments: [
       {
         id: "1",
         title: "Example",
         doctor: "Dr. Example",
-        date: "10/10/2024",
+        start: "10/10/2024",
+        end: "10/10/2024",
+        description: "This is an example appointment",
       },
     ],
     medications: [
@@ -40,32 +44,40 @@ export default function NewPet() {
             type="text"
             id="name"
             name="name"
-            value={name}
-            onChange={(e) => setName(e.currentTarget.value)}
+            value={newPetState.name}
+            onChange={(e) =>
+              setNewPetState({ ...newPetState, name: e.currentTarget.value })
+            }
           />
           <label htmlFor="species">Species</label>
           <input
             type="text"
             id="species"
             name="species"
-            value={species}
-            onChange={(e) => setSpecies(e.currentTarget.value)}
+            value={newPetState.species}
+            onChange={(e) =>
+              setNewPetState({ ...newPetState, species: e.currentTarget.value })
+            }
           />
           <label htmlFor="breed">Breed</label>
           <input
             type="text"
             id="breed"
             name="breed"
-            value={breed}
-            onChange={(e) => setBreed(e.currentTarget.value)}
+            value={newPetState.breed}
+            onChange={(e) =>
+              setNewPetState({ ...newPetState, breed: e.currentTarget.value })
+            }
           />
           <label htmlFor="age">Age</label>
           <input
             type="text"
             id="age"
             name="age"
-            value={age}
-            onChange={(e) => setAge(e.currentTarget.value)}
+            value={newPetState.age}
+            onChange={(e) =>
+              setNewPetState({ ...newPetState, age: e.currentTarget.value })
+            }
           />
           <button type="submit" className="mt-3">
             Add Pet
