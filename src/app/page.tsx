f9d5b313b22f6918f63df529bda3f394a4c5891b
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-02-11 12:58:22 +0200

    Landing page added credits and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,15 +32,17 @@ export default function Home() {
       <main>
         <div className="flex flex-row justify-around items-center">
           <div>
-            <p>
-              Manage your pet&apos;s medication and doctor appointments easily!
+            <p className="landing-text">
+              Keep track of your pet&apos;s health with ease! <br />
+              Pet Diary helps you manage vet visits, medications, and daily care
+              all in one place.
             </p>
           </div>
           <div className="landing-page-img">
             <img src="./landing-page.png" alt="Landing page image" />
           </div>
         </div>
-        <div>
+        <div className="flex flex-row justify-center items-center mt-20">
           <button
             onClick={() => handleGoogle()}
             className="flex flex-row justify-center items-center bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-200 rounded shadow"
@@ -56,6 +58,19 @@ export default function Home() {
           </button>
         </div>
       </main>
+      <div className="bottom-txt absolute">
+        <p>
+          Developed by
+          <a
+            href="https://www.linkedin.com/in/drivasgeorgios/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Georgios Drivas
+          </a>
+        </p>
+      </div>
     </>
   );
 }
