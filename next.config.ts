745ce60d37d791c3b35ec78527c4a1a415bd76c9
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-02-23 21:07:02 +0200

    tsconfig file changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Applies to all routes
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "unsafe-none", // Fixes COOP blocking issue
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
