f38febd6730273dce8db82b0b97770766516f878
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-02-25 18:47:23 +0200

    Refactor: month and day variable conditional check on formatDate

--- src/components/formatDate.tsx
+++ src/components/formatDate.tsx
@@ -12,7 +12,7 @@ export function convertDateToInputFormat(date: string): string {
     }
 
     const [day, month, year] = parts;
-    return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+    return `${year}-${month?.padStart(2, "0")}-${day?.padStart(2, "0")}`;
   } else if (date.includes("-")) {
     return date;
   }
