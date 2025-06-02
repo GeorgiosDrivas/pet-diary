90e93518bf01b78baa730d294133e44c765c9ed1
Georgios Drivas <78447254+GeorgiosDrivas@users.noreply.github.com>
2025-06-02 14:13:02 +0300

    Update layout.tsx to use fonts from fonts.ts

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,18 +3,7 @@ import localFont from "next/font/local";
 import "../globals.css";
 import React from "react";
 import Credits from "@/components/Credits";
-
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import {geistSans, geistMono} from "../fonts/fonts.ts";
 
 export const metadata: Metadata = {
   title: "Pet Diary",
