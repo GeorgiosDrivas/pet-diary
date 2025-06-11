0f780acac84d1c6e7fcab17f9b20405901a96c7d
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-06-11 20:43:16 +0300

    Resolve errors in jest setup

--- jest/jest.config.ts
+++ jest/jest.config.ts
@@ -1,12 +1,6 @@
-// jest.config.ts
-import nextJest from "next/jest";
-import { Config } from "jest";
-
-// const createJestConfig = nextJest({ dir: "./" });
-
 const customJestConfig = {
-  rootDir: "./",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  rootDir: "../",
+  setupFilesAfterEnv: ["<rootDir>/jest/jest.setup.ts"],
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "^firebase/client$": "<rootDir>/__mocks__/firebase/client.ts",
