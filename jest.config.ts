672cf85ae18f75dc18c577827720c205c652e506
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-06-11 13:18:50 +0300

    Setup Jest to test util functions

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,19 @@
 // jest.config.ts
 import nextJest from "next/jest";
+import { Config } from "jest";
 
-const createJestConfig = nextJest({ dir: "./" });
+// const createJestConfig = nextJest({ dir: "./" });
 
 const customJestConfig = {
+  rootDir: "./",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jsdom",
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+    "^firebase/client$": "<rootDir>/__mocks__/firebase/client.ts",
   },
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
 };
 
-export default createJestConfig(customJestConfig);
+export default customJestConfig;
