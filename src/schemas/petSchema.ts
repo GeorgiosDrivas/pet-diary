28d08de077f3dcb4c6d57bf2f2d7adfdee0df7ab
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-07-01 14:52:21 +0300

    Refactor pet zod schema

--- src/schemas/petSchema.ts
+++ src/schemas/petSchema.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const petSchema = z.object({
-  name: z.string(),
-  species: z.string(),
-  breed: z.string(),
-  age: z.string(),
+  name: z.string().min(3, "Name is required"),
+  species: z.string().min(3, "Species is required"),
+  breed: z.string().min(3, "Breed is required"),
+  age: z.string().min(1, "Age is required"),
 });
 
 export type PetFormData = z.infer<typeof petSchema>;
