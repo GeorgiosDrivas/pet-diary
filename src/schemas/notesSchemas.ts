b05f7d32bfe8e39d4e3d3bcb9a94cf822930afbf
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-07-02 23:59:19 +0300

    Add extra zod validation to schemas

--- src/schemas/notesSchemas.ts
+++ src/schemas/notesSchemas.ts
@@ -1,13 +1,12 @@
 import { z } from "zod";
 
 export const noteSchema = z.object({
-  id: z.number(),
-  name: z
-    .string()
-    .min(3, "Note name is recuired and must be at least 3 characters long"),
-  content: z
-    .string()
-    .min(5, "Note content is required and must be at least 5 characters long"),
+  id: z
+    .number()
+    .int("Age must be a whole number")
+    .positive("Age must be a positive number"),
+  name: z.string().min(3, "Note name must be at least 3 characters long"),
+  content: z.string().min(5, "Note content must be at least 5 characters long"),
 });
 
 export type noteSchemaType = z.infer<typeof noteSchema>;
--- src/schemas/petSchema.ts
+++ src/schemas/petSchema.ts
@@ -1,10 +1,14 @@
 import { z } from "zod";
 
 export const petSchema = z.object({
-  name: z.string().min(3, "Name is required"),
-  species: z.string().min(3, "Species is required"),
-  breed: z.string().min(3, "Breed is required"),
-  age: z.number().min(1, "Age is required"),
+  name: z.string().min(3, "Name must be at least 3 characters long"),
+  species: z.string().min(3, "Species must be at least 3 characters long"),
+  breed: z.string().min(3, "Breed must be at least 3 characters long"),
+  age: z
+    .number()
+    .min(1, "Age must be at least 1 year")
+    .int("Age must be a whole number")
+    .positive("Age must be a positive number"),
 });
 
 export type PetFormData = z.infer<typeof petSchema>;
