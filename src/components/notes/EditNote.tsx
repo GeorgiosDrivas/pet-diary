e403c0c409ffa90d8a8929371d26d388da177e65
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-07-03 12:48:10 +0300

    Edit note form zod validation

--- src/components/notes/EditNote.tsx
+++ src/components/notes/EditNote.tsx
@@ -1,7 +1,7 @@
 import { Pet } from "@/types";
 import React from "react";
 import { editNote } from "../../../firebase/editMethods";
-import { noteSchemaType } from "@/schemas/notesSchemas";
+import { noteSchema, noteSchemaType } from "@/schemas/notesSchemas";
 
 export default function EditNote({
   userId,
@@ -16,14 +16,22 @@ export default function EditNote({
   setEditable: React.Dispatch<React.SetStateAction<boolean>>;
   setNote: React.Dispatch<React.SetStateAction<noteSchemaType | null>>;
 }) {
+  const handleSubmit = () => {
+    const schemaResult = noteSchema.safeParse(Note);
+
+    if (schemaResult.success) {
+      if (Note) {
+        editNote(userId, pet.name, Note.id, Note);
+      }
+    }
+  };
+
   return (
     <>
       <div className="relative w-[300px]">
         <form
           onSubmit={() => {
-            if (Note) {
-              editNote(userId, pet.name, Note.id, Note);
-            }
+            handleSubmit();
           }}
         >
           <div>
